559ed563e94fbcf10ec9778bfba6932971a2b872
SJA <sascha.jannsen@smail.th-koeln.de>
2024-06-03 17:08:36 +0200

    feat(robot): #9 Add event listening for robot domain

--- src/event-handling/setup-event-listener.ts
+++ src/event-handling/setup-event-listener.ts
@@ -1,7 +1,9 @@
 import setUpPlanetEventListeners from '../modules/map/input/event-handling/planet-event-listener'
+import setUpRobotEventListeners from '../modules/robot/input/event-handling/robot-event-listener'
 import setUpTradingEventListeners from '../modules/trading/bank-account/input/event-handling/bankAccount-event-listener'
 
 export default function setUpEventListeners() {
   setUpTradingEventListeners()
   setUpPlanetEventListeners()
+  setUpRobotEventListeners()
 }
--- src/modules/robot/output/data-access/index.ts
+++ src/modules/robot/output/data-access/index.ts
@@ -1,11 +1,35 @@
 import { Robot } from '../../domain/models/robot'
-import { RobotDb } from '../../domain/use-cases/types'
+import { QueryParams, RobotDb } from '../../domain/use-cases/types'
+
+const robotData: Robot[] = []
+
+const matchesId = (robot: Robot, id: string | undefined) => !id || robot.getId() == id
+const matchesRobotServiceId = (robot: Robot, id: string | undefined) => !id || robot.getRobotServiceId() == id
+const matchesIds = (robot: Robot, ids: string[] | undefined) =>
+  !ids || ids.length == 0 || ids.includes(robot.getPlayer())
+
+function findRobot({ id, robotServiceId }: QueryParams) {
+  return robotData.find((robot) => matchesId(robot, id) && matchesRobotServiceId(robot, robotServiceId))
+}
+
+function findAll(queryParams?: QueryParams) {
+  return robotData.filter((r) => matchesIds(r, queryParams?.playerIds))
+}
 
 const robotDb: RobotDb = {
-  find: () => ({}) as Robot,
-  findAll: () => [],
-  insert: () => ({}) as Robot,
-  update: () => ({}) as Robot,
+  find: findRobot,
+  findAll,
+  insert: (robot: Robot) => {
+    robotData.push(robot)
+    return robot
+  },
+  update: (robot: Robot) => {
+    const index = robotData.findIndex(
+      (r) => r.getId() == robot.getId() && r.getRobotServiceId() == robot.getRobotServiceId()
+    )
+    robotData[index] = robot
+    return robot
+  },
 }
 
 export default robotDb
