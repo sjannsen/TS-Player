cb5756fd13e8bdadca15681282575be674ccce12
Sascha <92031835+sjannsen@users.noreply.github.com>
2024-06-16 21:25:07 +0200

    refactor(mongoDB): Split mongoDB connection into initialize and get function #83

--- src/modules/robot/adapters/output/data-access/index.ts
+++ src/modules/robot/adapters/output/data-access/index.ts
@@ -1,5 +1,5 @@
-import { connectToMongoDB } from '../../../../../db/mongoDB-connection'
+import { getMongoDBConnection } from '../../../../../db/mongoDB-connection'
 import makeRobotsDatabase from './robots-database'
 
-const robotsDb = makeRobotsDatabase({ makeDb: connectToMongoDB })
+const robotsDb = makeRobotsDatabase({ makeDb: getMongoDBConnection })
 export default robotsDb
--- src/modules/trading/item/adapters/output/data-access/index.ts
+++ src/modules/trading/item/adapters/output/data-access/index.ts
@@ -1,7 +1,7 @@
-import { connectToMongoDB } from '../../../../../../db/mongoDB-connection'
+import { getMongoDBConnection } from '../../../../../../db/mongoDB-connection'
 import { ItemsDatabase } from '../../../domain/use-cases/data-access'
 import makeItemsDatabase from './items-database'
 
-const itemsDatabase: ItemsDatabase = makeItemsDatabase({ makeDb: connectToMongoDB })
+const itemsDatabase: ItemsDatabase = makeItemsDatabase({ makeDb: getMongoDBConnection })
 
 export default itemsDatabase
