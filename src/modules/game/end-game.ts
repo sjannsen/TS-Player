e743b005e8d9cf64206fb6a128ea244aa368b0b7
Sascha <92031835+sjannsen@users.noreply.github.com>
2024-06-20 11:17:08 +0200

    feat(game): Remove service shutdown on game end #90

--- src/modules/game/end-game.ts
+++ src/modules/game/end-game.ts
@@ -1,13 +1,11 @@
-import { closeConnectionToNeo4j } from '../../db/neo4j-connection'
 import eventBus from '../../event-handling/event-bus'
 import { EventContext } from '../../event-handling/events'
+import logger from '../../utils/logger'
 
 export default function setUpGameEndListener() {
   eventBus.subscribe('GameStatus', async ({ event }: EventContext<'GameStatus'>) => {
     if (event.payload.status === 'ended') {
-      console.log('Game ended, shutting down service... 👌☕')
-      await closeConnectionToNeo4j()
-      process.exit(0)
+      logger.info('Game ended, shutting down service... 👌☕')
     }
   })
 }
