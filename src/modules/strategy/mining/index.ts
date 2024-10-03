7b2a34cb185d1e88b8c7f5b0339ed6049af9de5c
Sascha <92031835+sjannsen@users.noreply.github.com>
2024-10-03 19:05:25 +0200

    #75 style(strategy): Format document

--- src/modules/strategy/mining/index.ts
+++ src/modules/strategy/mining/index.ts
@@ -1,9 +1,15 @@
-import { RobotData } from "../../robot/domain/models/robot";
-import bankAccountService from "../../trading/bank-account/domain/use-cases";
-import itemService from "../../trading/item/domain/use-cases";
-import { buyUpgrade } from "../../trading/upgrade/output/commands/buyUpgrade";
-import makeBuyMiningLevelUpgrade from "./buy-mining-level-upgrade";
+import { RobotData } from '../../robot/domain/models/robot'
+import bankAccountService from '../../trading/bank-account/domain/use-cases'
+import itemService from '../../trading/item/domain/use-cases'
+import { buyUpgrade } from '../../trading/upgrade/output/commands/buyUpgrade'
+import makeBuyMiningLevelUpgrade from './buy-mining-level-upgrade'
 
-const buyMiningLevelUpgrade = ({ robot }: { robot: RobotData}) => makeBuyMiningLevelUpgrade({ robot, buyUpgrade: buyUpgrade, bankAccountService: bankAccountService, itemService: itemService })
+const buyMiningLevelUpgrade = ({ robot }: { robot: RobotData }) =>
+  makeBuyMiningLevelUpgrade({
+    robot,
+    buyUpgrade: buyUpgrade,
+    bankAccountService: bankAccountService,
+    itemService: itemService,
+  })
 
 export { buyMiningLevelUpgrade }
