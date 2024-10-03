2d1f2d1c67c8803745a0ddc05d7fc174ff7970a4
Sascha <92031835+sjannsen@users.noreply.github.com>
2024-10-03 19:15:27 +0200

    #100 fix(commands): Add planetId to sellResources command

--- src/modules/trading/inventory/adapters/output/commands/sellRessources.ts
+++ src/modules/trading/inventory/adapters/output/commands/sellRessources.ts
@@ -1,6 +1,6 @@
 import { SellTradablesCommandData, SellTradablesCommand } from '../../../../../../shared/commands'
 import { sendCommand } from '../../../../../../shared/sendCommand'
 
-export async function sellRessources({ robotId }: SellTradablesCommandData) {
-  return sendCommand<SellTradablesCommand>({ type: 'selling', data: { robotId } })
+export async function sellRessources({ robotId, planetId }: SellTradablesCommandData) {
+  return sendCommand<SellTradablesCommand>({ type: 'selling', data: { robotId, planetId } })
 }
