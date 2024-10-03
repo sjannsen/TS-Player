260655b61875ec4035658ea621def1cb99d9312d
Sascha <92031835+sjannsen@users.noreply.github.com>
2024-10-03 19:16:17 +0200

    #100 refactor(strategy): Fix spelling error

--- src/modules/trading/inventory/adapters/output/commands/sellResources.ts
+++ src/modules/trading/inventory/adapters/output/commands/sellResources.ts
@@ -1,6 +1,6 @@
 import { SellTradablesCommandData, SellTradablesCommand } from '../../../../../../shared/commands'
 import { sendCommand } from '../../../../../../shared/sendCommand'
 
-export async function sellRessources({ robotId, planetId }: SellTradablesCommandData) {
+export async function sellResources({ robotId, planetId }: SellTradablesCommandData) {
   return sendCommand<SellTradablesCommand>({ type: 'selling', data: { robotId, planetId } })
 }
