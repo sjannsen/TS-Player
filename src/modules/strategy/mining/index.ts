bf3cfc5ae963e69b08c8df64f300828a4f69837b
Sascha <92031835+sjannsen@users.noreply.github.com>
2024-10-03 18:42:21 +0200

    #75 refactor(strategy): Fix spelling error

--- src/modules/strategy/mining/index.ts
+++ src/modules/strategy/mining/index.ts
@@ -1,6 +1,6 @@
 import { RobotData } from "../../robot/domain/models/robot";
 import { buyUpgrade } from "../../trading/upgrade/output/commands/buyUpgrade";
-import makeBuyMiningLevelUpgrade from "./buy-mining-leve-upgrade";
+import makeBuyMiningLevelUpgrade from "./buy-mining-level-upgrade";
 
 const buyMiningLevelUpgrade = ({ robot }: { robot: RobotData}) => makeBuyMiningLevelUpgrade({ robot, buyUpgrade: buyUpgrade })
 
