81fdba6b11aed5cc1fe7a000d9dc18462c56c3cd
Sascha <92031835+sjannsen@users.noreply.github.com>
2024-10-04 03:22:12 +0200

    #122 feat(strategy): Constantly buy new robots

--- src/modules/strategy/trading/trading-strategy.ts
+++ src/modules/strategy/trading/trading-strategy.ts
@@ -1,10 +1,17 @@
 import logger from '../../../utils/logger'
 import { getCurrentRoundNumber } from '../../game/roundStatus'
 import { buyRobots } from '../../robot/adapters/output/commands'
+import robotService from '../../robot/domain/use-cases'
+import bankAccountService from '../../trading/bank-account/domain/use-cases'
+import itemService from '../../trading/item/domain/use-cases'
+
+const ROBOT_LIMIT = 100
 
 export default async function getTradingStrategy() {
   const roundNumber = getCurrentRoundNumber()
-  if (roundNumber === 2) getFirstRoundStrategy()
+  if (roundNumber === 2) return getFirstRoundStrategy()
+
+  getFollowingRoundsStrategy()
 }
 
 async function getFirstRoundStrategy() {
@@ -13,5 +20,34 @@ async function getFirstRoundStrategy() {
     logger.info('Bought initial robots 🤖🔪')
   } catch (error) {
     logger.error({ error }, 'An error ocurred while buying robots')
+  }
+}
+
+async function getFollowingRoundsStrategy() {
+  const currentBalance = bankAccountService.getBalance()
+  const robotItem = await itemService.findByName({ itemName: 'ROBOT' })
+  const miningUpgrade = await itemService.findByName({ itemName: 'MINING_3' })
+
+  if (!robotItem) {
+    logger.error({ robotPrice: robotItem, currentBalance }, 'Robot Item is undefined 🤖🛑')
+    throw new Error('Robot item undefined 🤖🛑')
+  }
+
+  if (!miningUpgrade) {
+    logger.error({ miningUpgrade, currentBalance }, 'MiningUpgrade Item is undefined ⛏️🛑')
+    throw new Error('MiningUpgrade item undefined ⛏️🛑')
+  }
+
+  const robotCount = (await robotService.listRobots()).length
+  const canBuyMiningUpgrade = currentBalance <= miningUpgrade.price + 1
+  if (ROBOT_LIMIT <= robotCount || canBuyMiningUpgrade) return
+
+  const robotPrice = robotItem.price
+  const spendingLimit = currentBalance * 0.5
+  const buyableAmount = Math.floor(spendingLimit / robotPrice)
+
+  if (buyableAmount > 0) {
+    logger.error(`Going to buy ${buyableAmount} Robots 🤖`)
+    await buyRobots(buyableAmount)
   }
 }
